20bf0bea325121fc1c5666aa38954a41c969f2cb
Aarush Gupta <71131751+aarush-glitch@users.noreply.github.com>
2025-06-07 18:49:26 +0530

    Create TrappingRainWater.cpp

---
+++ Stacks & Queues/TrappingRainWater.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& arr) {
+        int n=arr.size();
+        int l=0, r=n-1;
+        int lMax=0, rMax=0, total=0;
+
+        while(l<r) {
+            if(arr[l] <= arr[r]) {
+                if(lMax > arr[l]) total += lMax-arr[l];
+                else lMax = arr[l];
+                l++;
+            }
+            else {
+                if(rMax > arr[r]) total += rMax - arr[r];
+                else rMax = arr[r];
+                r--;;
+            }
+        }
+        return total;
+    }
+};
