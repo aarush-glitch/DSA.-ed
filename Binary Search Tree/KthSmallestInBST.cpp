adde767b154b464aa260159a881d44179a1333f8
Aarush Gupta <71131751+aarush-glitch@users.noreply.github.com>
2025-08-07 12:45:38 +0530

    Create KthSmallestInBST.cpp

---
+++ Binary Search Tree/KthSmallestInBST.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> s;
+        TreeNode* node = root;
+
+        while(node || !s.empty()) {
+            while(node) {
+                s.push(node);
+                node=node->left;
+            }
+            node=s.top();
+            s.pop();
+            k--;
+            if(k==0) return node->val;
+            node=node->right;
+        }
+        return -1;
+    }
+};
