6e1297c4de0fdf504878ad3b49bece8fc6b48f65
Aarush Gupta <71131751+aarush-glitch@users.noreply.github.com>
2025-09-29 00:53:40 +0530

    Create AccountsMerge.cpp

---
+++ Graph/AccountsMerge.cpp
@@ -0,0 +1,73 @@
+class DisjointSet {
+    vector<int> size, parent;
+public:
+    DisjointSet(int n) {
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+
+        for(int i=0; i<=n; i++) parent[i]=i;
+    }
+
+    int findPar(int node) {
+        if(parent[node]==node) return node;
+
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findPar(u), ulp_v = findPar(v);
+        if(ulp_u == ulp_v) return;
+
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        DisjointSet ds(n);
+        sort(accounts.begin(), accounts.end());
+        unordered_map<string, int> mapMailNode;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=1; j<accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+                if(mapMailNode.find(mail)==mapMailNode.end()) {
+                    mapMailNode[mail] = i;
+                }
+                else {
+                    ds.unionBySize(i, mapMailNode[mail]);
+                }
+            }
+        }
+
+        vector<vector<string>> mergedMail(n);
+        for(auto it: mapMailNode) {
+            string mail = it.first;
+            int node = ds.findPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+
+        vector<vector<string>> ans;
+
+        for(int i=0; i<n; i++) {
+            if(mergedMail[i].size()==0) continue;
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it: mergedMail[i]) {
+                temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
